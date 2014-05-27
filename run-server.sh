b665106eeac1f1265b1f31bc4a9eabac590662b2
Stefan Kühn <ducdebreme@early-dance.de>
2014-05-27 15:30:01 +0200

    optimized mysql settings

--- run-server.sh
+++ run-server.sh
@@ -4,7 +4,7 @@
 mkdir -p data-mysql
 
 # as daemon
-docker run -d -p 3306:3306 -v  $(pwd)/data-mysql:/var/lib/mysql -e MYSQL_PASS="samba" tutum/mysql 
+docker run --name mysql -d -p 3306:3306 -v  $(pwd)/data-mysql:/var/lib/mysql -e MYSQL_PASS="samba" tutum/mysql 
 
 # interactive
-docker run -i -t -p 3306:3306 -v  $(pwd)/data-mysql:/var/lib/mysql -e MYSQL_PASS="samba" tutum/mysql bash
+#docker run -i -t -p 3306:3306 -v  $(pwd)/data-mysql:/var/lib/mysql -e MYSQL_PASS="samba" tutum/mysql bash
