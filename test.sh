8222110436e0c9e4d1abe48b2058cb77fb995348
BR <borja@libcrack.so>
2015-10-15 14:15:18 +0200

    New: Added custom testing function "mysql_docker_test_release" to test.sh

--- test.sh
+++ test.sh
@@ -1,51 +1,58 @@
 #!/bin/bash
+# jue oct 15 13:18:38 CEST 2015
+# borja@libcrack.so
 
 set -e
 
-echo "=> Building mysql 5.5 image"
-docker build -t mysql-5.5 5.5/
+mysql_docker_test_release()
+{
+    local major="5"
+    local minor="7"
 
-echo "=> Testing if mysql is running on 5.5"
-docker run -d -p 13306:3306 -e MYSQL_USER="user" -e MYSQL_PASS="test" mysql-5.5; sleep 10
-mysqladmin -uuser -ptest -h127.0.0.1 -P13306 ping | grep -c "mysqld is alive"
+    [[ -z "$major" ]] || major="$1"
+    [[ -z "$minor" ]] || major="$2"
 
-echo "=> Testing replication on mysql 5.5"
-docker run -d -e MYSQL_USER=user -e MYSQL_PASS=test -e REPLICATION_MASTER=true -e REPLICATION_USER=repl -e REPLICATION_PASS=repl -p 13307:3306 --name mysql55master mysql-5.5; sleep 10
-docker run -d -e MYSQL_USER=user -e MYSQL_PASS=test -e REPLICATION_SLAVE=true -p 13308:3306 --link mysql55master:mysql mysql-5.5; sleep 10
-docker logs mysql55master | grep "repl:repl"
-mysql -uuser -ptest -h127.0.0.1 -P13307 -e "show master status\G;" | grep "mysql-bin.*"
-mysql -uuser -ptest -h127.0.0.1 -P13308 -e "show slave status\G;" | grep "Slave_IO_Running.*Yes"
-mysql -uuser -ptest -h127.0.0.1 -P13308 -e "show slave status\G;" | grep "Slave_SQL_Running.*Yes"
+    local release="${major}.${minor}"
 
-echo "=> Testing volume on mysql 5.5"
-mkdir vol55
-docker run --name mysql55.1 -d -p 13309:3306 -e MYSQL_USER="user" -e MYSQL_PASS="test" -v $(pwd)/vol55:/var/lib/mysql mysql-5.5; sleep 10
-mysqladmin -uuser -ptest -h127.0.0.1 -P13309 ping | grep -c "mysqld is alive"
-docker stop mysql55.1
-docker run  -d -p 13310:3306 -v $(pwd)/vol55:/var/lib/mysql mysql-5.5; sleep 10
-mysqladmin -uuser -ptest -h127.0.0.1 -P13310 ping | grep -c "mysqld is alive"
+    local lport="$(($RANDOM+1024))"
+    local lport_master="$(($lport+1))"
+    local lport_slave="$(($lport+2))"
+    local lport_vol="$(($lport+3))"
+    local lport_vol2="$(($lport+4))"
 
-echo "=> Building mysql 5.6 image"
-docker build -t mysql-5.6 5.6/
+    echo -e "\e[1m=> Building mysql ${release} image\e[0m"
+    docker build -t mysql-${release} ${release}/
 
-echo "=> Testing if mysql is running on 5.6"
-docker run -d -p 23306:3306 -e MYSQL_USER="user" -e MYSQL_PASS="test" mysql-5.6; sleep 10
-mysqladmin -uuser -ptest -h127.0.0.1 -P13307 ping | grep -c "mysqld is alive"
+    echo -e "\e[1m=> Testing if mysql is running on ${release}"
+    docker run -d -p ${lport}:3306 -e MYSQL_USER="user" -e MYSQL_PASS="test" mysql-${release}
+    sleep 10
+    mysqladmin -uuser -ptest -h127.0.0.1 -P${lport} ping | grep -c "mysqld is alive"
 
-echo "=> Testing replication on mysql 5.6"
-docker run -d -e MYSQL_USER=user -e MYSQL_PASS=test -e REPLICATION_MASTER=true -e REPLICATION_USER=repl -e REPLICATION_PASS=repl -p 23307:3306 --name mysql56master mysql-5.6; sleep 10
-docker run -d -e MYSQL_USER=user -e MYSQL_PASS=test -e REPLICATION_SLAVE=true -p 23308:3306 --link mysql56master:mysql mysql-5.6; sleep 10
-docker logs mysql56master | grep "repl:repl"
-mysql -uuser -ptest -h127.0.0.1 -P23307 -e "show master status\G;" | grep "mysql-bin.*"
-mysql -uuser -ptest -h127.0.0.1 -P23308 -e "show slave status\G;" | grep "Slave_IO_Running.*Yes"
-mysql -uuser -ptest -h127.0.0.1 -P23308 -e "show slave status\G;" | grep "Slave_SQL_Running.*Yes"
+    echo -e "\e[1m=> Testing replication on mysql ${release}\e[0m"
+    docker run -d -e MYSQL_USER=user -e MYSQL_PASS=test -e REPLICATION_MASTER=true \
+        -e REPLICATION_USER=repl -e REPLICATION_PASS=repl -p ${lport_master}:3306 \
+        --name mysql${release//.}master mysql-${release}; sleep 10
+    docker run -d -e MYSQL_USER=user -e MYSQL_PASS=test -e REPLICATION_SLAVE=true \
+        -p ${lport_slave}:3306 --link mysql${release//.}master:mysql mysql-${release}
+    sleep 10
+    docker logs mysql${release//.}master | grep "repl:repl"
+    mysql -uuser -ptest -h127.0.0.1 -P${lport_master} -e "show master status\G;" | grep "mysql-bin.*"
+    mysql -uuser -ptest -h127.0.0.1 -P${lport_slave} -e "show slave status\G;" | grep "Slave_IO_Running.*Yes"
+    mysql -uuser -ptest -h127.0.0.1 -P${lport_slave} -e "show slave status\G;" | grep "Slave_SQL_Running.*Yes"
 
-echo "=> Testing volume on mysql 5.6"
-mkdir vol56
-docker run --name mysql56.1 -d -p 23309:3306 -e MYSQL_USER="user" -e MYSQL_PASS="test" -v $(pwd)/vol56:/var/lib/mysql mysql-5.6; sleep 10
-mysqladmin -uuser -ptest -h127.0.0.1 -P23309 ping | grep -c "mysqld is alive"
-docker stop mysql56.1
-docker run  -d -p 23310:3306 -v $(pwd)/vol56:/var/lib/mysql mysql-5.6; sleep 10
-mysqladmin -uuser -ptest -h127.0.0.1 -P23310 ping | grep -c "mysqld is alive"
+    echo -e "\e[1m=> Testing volume on mysql ${release}\e[0m"
+    mkdir vol${release//.}
+    docker run --name mysql${release//.}.1 -d -p ${lport_vol}:3306 -e MYSQL_USER="user" -e MYSQL_PASS="test" \
+        -v $(pwd)/vol${release//.}:/var/lib/mysql mysql-${release}
+    sleep 10
+    mysqladmin -uuser -ptest -h127.0.0.1 -P${lport_vol} ping | grep -c "mysqld is alive"
+    docker stop mysql${release//.}.1
+    docker run  -d -p ${lport_vol2}:3306 -v $(pwd)/vol${release//.}:/var/lib/mysql mysql-${release}; sleep 10
+    mysqladmin -uuser -ptest -h127.0.0.1 -P${lport_vol2} ping | grep -c "mysqld is alive"
+}
 
-echo "=>Done"
+mysql_docker_test_release 5 5
+mysql_docker_test_release 5 6
+mysql_docker_test_release 5 7
+
+echo -e "\e[1m DONE \e[0m"
